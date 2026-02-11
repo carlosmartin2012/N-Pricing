e0b15496f4507315d3614d8d4f2b84547f88c974
carlosmartin2012 <carlos.martin@nfq.es>
2026-02-11 17:27:25 +0100

    Logo Actualizado

--- components/ui/Logo.tsx
+++ components/ui/Logo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const Logo: React.FC<{ className?: string }> = ({ className = "w-8 h-8" }) => (
   <img
-    src="/assets/logo_n_pricing.png"
+    src="/assets/logo_n_latest.jpg"
     alt="Logo"
     className={`${className} object-contain`}
     onError={(e) => (e.currentTarget.style.display = 'none')}
